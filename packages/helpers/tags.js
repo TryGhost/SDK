bb8f40f642572049374a35343c172583495a571a
Hannah Wolfe <erisds@gmail.com>
2018-10-08 13:59:24 -0400

    ✨ Support fn creating both strings and objects
    - If we're working in handlebars, we're probably working with strings
    - If we're in react we're possibly working with strings or react element objects
    - This version works for both
    - @TODO: tests!

--- packages/helpers/tags.js
+++ packages/helpers/tags.js
@@ -7,11 +7,11 @@ const visibility = require('./utils/visibility');
  * @param {object} options - filter options
  */
 
-module.exports = (data, options) => {
+module.exports = (data, options = {}) => {
     let output = '';
-    let separator = _.isString(options.separator) ? options.separator : ', ';
-    let prefix = _.isString(options.prefix) ? options.prefix : '';
-    let suffix = _.isString(options.suffix) ? options.suffix : '';
+    let separator = options.separator ? options.separator : ', ';
+    let prefix = options.prefix ? options.prefix : '';
+    let suffix = options.suffix ? options.suffix : '';
     let limit = options.limit ? parseInt(options.limit, 10) : undefined;
     let from = options.from ? parseInt(options.from, 10) : 1;
     let to = options.to ? parseInt(options.to, 10) : undefined;
@@ -28,11 +28,29 @@ module.exports = (data, options) => {
 
         from -= 1; // From uses 1-indexed, but array uses 0-indexed.
         to = to || limit + from || output.length;
-        output = output.slice(from, to).join(separator);
+        output = output.slice(from, to);
     }
 
-    if (output) {
-        output = prefix + output + suffix;
+    // If we have a result from the filtering process...
+    if (_.size(output) > 0) {
+        // Check to see if options.fn returned a string, or something else
+        if (_.isString(output[0])) {
+            // If we're working with a string, do a simple join and string-concat
+            output = prefix + output.join(separator) + suffix;
+        } else {
+            // Else, operate on the array, and return an array
+            if (separator) {
+                // If we have a separator, use lodash to make pairs of items & separators
+                output = _.zip(output, _.fill(Array(output.length), separator));
+                // Flatten our pairs, and remove the final separator
+                output = _.flatten(output).slice(0, -1);
+            }
+
+            // Add our prefix and suffix
+            output = _.concat(prefix, output, suffix);
+            // Remove any falsy items after all that (i.e. if prefix/suffix were empty);
+            output = _.compact(output);
+        }
     }
 
     return output;
