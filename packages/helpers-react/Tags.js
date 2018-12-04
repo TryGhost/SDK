66c9550f6db4755ac1068e21dcfe9c35203b90cc
Hannah Wolfe <erisds@gmail.com>
2018-12-04 17:55:21 +0000

    🛠 Sort out react linting

--- packages/helpers-react/Tags.js
+++ packages/helpers-react/Tags.js
@@ -16,16 +16,18 @@ import tagsHelper from '@tryghost/helpers/tags'
 *   - separatorClasses [optional, default "", classNames used for the html separator tags]
 *   - prefixClasses [optional, default "", classNames used for the html prefix tags]
 *   - suffixClasses [optional, default "", classNames used for the html suffix tags]
+*   - linkClasses [optional, default "", classNames used for the html link tags]
+*   - autolink [optional, default true, whether to convert tags to links]
+*   - permalink [optional, default "/:slug.", the pattern used for links]
 */
 const Tags = (props) => {
-    let post = props.post
     let opts = {
         limit: props.limit,
         from: props.from,
         to: props.to,
         fallback: props.fallback,
-        visibility: props.visibility
-    };
+        visibility: props.visibility,
+    }
 
     if (props.separator) {
         opts.separator = React.isValidElement(props.separator) ? props.separator :
@@ -42,17 +44,19 @@ const Tags = (props) => {
             <span className={props.suffixClasses}>{props.suffix}</span>
     }
 
-
     opts.fn = function process(tag) {
+        let tagLink = props.permalink
+        tagLink = tagLink.replace(/:slug/, tag.slug) || `/${tag.slug}/`
+
         return props.autolink ?
-            <span className={props.classes} key={props.slug}>
-                <Link to={props.permalink.replace(/:slug/, tag.slug)} className={props.linkClasses}>{tag.name}</Link>
+            <span className={props.classes} key={tag.slug}>
+                <Link to={tagLink} className={props.linkClasses}>{tag.name}</Link>
             </span> :
             <span className={props.classes} key={tag.slug}>{tag.name}</span>
     }
 
     return (
-        tagsHelper(post, opts)
+        tagsHelper(props.post, opts)
     )
 }
 
@@ -77,13 +81,13 @@ Tags.propTypes = {
     to: PropTypes.number,
     fallback: PropTypes.object,
     visibility: PropTypes.string,
+    permalink: PropTypes.string,
+    autolink: PropTypes.bool,
     classes: PropTypes.string,
     separatorClasses: PropTypes.string,
     prefixClasses: PropTypes.string,
     suffixClasses: PropTypes.string,
     linkClasses: PropTypes.string,
-    permalink: PropTypes.string,
-    autolink: PropTypes.boolean
 }
 
 export default Tags
