befd991446dbd93b24187b09319b1bf03d76ee7c
Fabien O'Carroll <fabien@allou.is>
2018-12-10 13:02:00 +0700

    Updated layer2 to resolve with success for auth page methods

--- packages/members/layer2/index.js
+++ packages/members/layer2/index.js
@@ -29,43 +29,36 @@ module.exports = function layer2(options) {
 
     function openAuth(hash, query = '') {
         return loadAuth.then(function (frame) {
-            frame.src = `${authUrl}#${hash}?${query}`;
-            frame.style.display = 'block';
-            window.addEventListener('message', function self(event) {
-                if (event.source !== frame.contentWindow) {
-                    return;
-                }
-                if (event.data !== 'pls-close-auth-popup') {
-                    return;
-                }
-                window.removeEventListener('message', self);
-                frame.style.display = 'none';
-            })
-            return frame;
+            return new Promise(function (resolve) {
+                frame.src = `${authUrl}#${hash}?${query}`;
+                frame.style.display = 'block';
+                window.addEventListener('message', function messageListener(event) {
+                    if (event.source !== frame.contentWindow) {
+                        return;
+                    }
+                    if (event.data !== 'pls-close-auth-popup') {
+                        return;
+                    }
+                    window.removeEventListener('message', messageListener);
+                    frame.style.display = 'none';
+                    resolve(false);
+                })
+                members.bus.on('signedin', function signedinListener() {
+                    members.bus.off('signedin', signedinListener);
+                    frame.style.display = 'none';
+                    resolve(true);
+                });
+            });
         });
     }
 
     function resetPassword({token}) {
         const query = `token=${token}`;
-        return openAuth('reset-password', query).then(function () {
-            return new Promise(function (resolve) {
-                members.bus.on('signedin', function self() {
-                    members.bus.off('signedin', self);
-                    resolve(true);
-                });
-            });
-        });
+        return openAuth('reset-password', query);
     }
 
     function signin() {
-        return openAuth('signin').then(function () {
-            return new Promise(function (resolve) {
-                members.bus.on('signedin', function self() {
-                    members.bus.off('signedin', self);
-                    resolve(true);
-                });
-            });
-        });
+        return openAuth('signin');
     }
 
     function getToken({audience}) {
