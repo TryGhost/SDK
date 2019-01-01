9c3967fe1f350e6afe7d055d71cf95ec39ae8f22
Hannah Wolfe <erisds@gmail.com>
2019-01-01 22:16:44 +0000

    🐛 Fixed react linting for helpers-gatsby

--- packages/helpers-gatsby/.eslintrc.js
+++ packages/helpers-gatsby/.eslintrc.js
@@ -1,6 +1,37 @@
 module.exports = {
-    plugins: ['ghost'],
+    plugins: ['ghost', 'react'],
     extends: [
-        'plugin:ghost/node',
-    ]
+        'plugin:ghost/browser',
+        'plugin:react/recommended'
+    ],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        },
+    },
+    settings: {
+        react: {
+            createClass: 'createReactClass',
+            pragma: 'React',
+            version: '16.0',
+            flowVersion: '0.53'
+        },
+    },
+    rules: {
+        semi: ['error', 'never'],
+        'object-curly-spacing': ['error', 'always'],
+         'comma-dangle': [
+             'error',
+             {
+                arrays: 'always-multiline',
+                objects: 'always-multiline',
+                imports: 'always-multiline',
+                exports: 'always-multiline',
+                functions: 'ignore'
+            }
+        ],
+        // Not sure why this isn't working
+        'react/prop-types': ['off']
+    }
 };
