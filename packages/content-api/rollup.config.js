1218fb6739c852829bf4bf779951421a1ca26f68
Fabien O'Carroll <fabien@allou.is>
2019-01-04 23:08:38 +0700

    Fixed process.env in the umd build

--- packages/content-api/rollup.config.js
+++ packages/content-api/rollup.config.js
@@ -2,48 +2,52 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import {terser} from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
 import pkg from './package.json';
 
 const dependencies = Object.keys(pkg.dependencies);
 
 export default [
-	// Node build.
-	// No transpilation or bundling other than converstion from es modules to cjs
-	{
-		input: pkg.source,
-		output: {
-			file: pkg.main,
-			format: 'cjs',
-			interop: false
-		},
+    // Node build.
+    // No transpilation or bundling other than converstion from es modules to cjs
+    {
+        input: pkg.source,
+        output: {
+            file: pkg.main,
+            format: 'cjs',
+            interop: false
+        },
         plugins: [
             commonjs({
                 include: 'node_modules/**'
             })
         ]
-	},
+    },
 
-	// Standalone UMD browser build (minified).
-	// Transpiles to es5 and bundles all dependencies.
-	{
-		input: pkg.source,
-		output: {
-			file: pkg['umd:main'],
-			format: 'umd',
-			name: '@tryghost/content-api'
-		},
-		plugins: [
-			resolve({
-				browser: true,
-				preferBuiltins: false
-			}),
+    // Standalone UMD browser build (minified).
+    // Transpiles to es5 and bundles all dependencies.
+    {
+        input: pkg.source,
+        output: {
+            file: pkg['umd:main'],
+            format: 'umd',
+            name: 'ContentApi'
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                preferBuiltins: false
+            }),
             commonjs({
                 include: 'node_modules/**'
             }),
-			babel({
-				exclude: 'mode_modules/**'
-			}),
-			terser()
-		]
-	}
+            babel({
+                exclude: 'mode_modules/**'
+            }),
+            replace({
+                'process.env.NODE_ENV': process.env.NODE_ENV
+            }),
+            terser()
+        ]
+    }
 ];
