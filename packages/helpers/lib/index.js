4f778477b666b456c54e5f0b40b89c6d237c1845
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-09 15:33:54 +0000

    ✨ Add `readingTime` helper to `@tryghost/helpers` (#41)
    no issue
    - adds `readingTime` helper, replicating `{{reading_time}}` helper output from Ghost
    - adds `utils.countWords` which returns number of words in text/html
    - adds `utils.countImages` which returns number of words in html
    
    Changes from Ghost's helper:
    - no use of `SafeString`, it's up to the consumer to wrap the output if necessary in whichever method is suitable for it's environment (react would be different to hbs for example)
    - returns a blank string rather than `null` if the passed in object does not resemble a post so that the helper's return type is consistent

--- packages/helpers/lib/index.js
+++ packages/helpers/lib/index.js
@@ -1,7 +1,12 @@
 import * as visibility from './utils/visibility';
+import countWords from './utils/count-words';
+import countImages from './utils/count-images';
 
 export const utils = {
+    countImages,
+    countWords,
     visibility
 };
 
+export {default as readingTime} from './reading-time';
 export {default as tags} from './tags';
