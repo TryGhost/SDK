9312c72489c46029a662bbf9de549b1a7232c94b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-14 10:03:54 +0000

    [admin-api] Removed UMD build
    closes https://github.com/TryGhost/Ghost-SDK/issues/47
    - admin API is not designed to be used client-side so we shouldn't provide a client-focused UMD build

--- packages/admin-api/rollup.config.js
+++ packages/admin-api/rollup.config.js
@@ -1,10 +1,5 @@
 /* eslint-env node */
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import builtins from 'rollup-plugin-node-builtins';
-import {terser} from 'rollup-plugin-terser';
-import replace from 'rollup-plugin-replace';
 import pkg from './package.json';
 
 const dependencies = Object.keys(pkg.dependencies);
@@ -25,33 +20,5 @@ export default [
             })
         ],
         external: dependencies
-    },
-
-    // Standalone UMD browser build (minified).
-    // Transpiles to es5 and bundles all dependencies.
-    {
-        input: pkg.source,
-        output: {
-            file: pkg['umd:main'],
-            format: 'umd',
-            name: 'GhostAdminAPI'
-        },
-        plugins: [
-            resolve({
-                browser: true,
-                preferBuiltins: false
-            }),
-            builtins(),
-            commonjs({
-                include: ['node_modules/**', '../../node_modules/**']
-            }),
-            babel({
-                exclude: ['node_modules/**', '../../node_modules/**']
-            }),
-            replace({
-                'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
-            }),
-            terser()
-        ]
     }
 ];
