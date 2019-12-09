e323b69261dc46bb4397c7ce9f622782b20e6e71
Nazar Gargol <nazargargol@gmail.com>
2019-12-09 11:54:38 +0700

    Removed redundant key check condition
    - The condition above it already check for this exact case

--- packages/admin-api/lib/index.js
+++ packages/admin-api/lib/index.js
@@ -63,7 +63,7 @@ module.exports = function GhostAdminAPI(options) {
     if (!config.key) {
         throw new Error('GhostAdminAPI Config Invalid: @tryghost/admin-api requires a "key" to be supplied');
     }
-    if (config.key && !/[0-9a-f]{24}:[0-9a-f]{64}/.test(config.key)) {
+    if (!/[0-9a-f]{24}:[0-9a-f]{64}/.test(config.key)) {
         throw new Error('GhostAdminAPI Config Invalid: @tryghost/admin-api requires a "key" in following format {A}:{B}, where A is 24 hex characters and B is 64 hex characters');
     }
 
