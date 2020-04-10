99b2adc1c82eface95dca2a85d6f62169265d3e4
Hannah Wolfe <erisds@gmail.com>
2020-04-10 08:06:51 +0100

    Moved helper util tests into Ghost-SDK from Ghost
    - these tests were left over in Ghost in the wrong place so they got missed;
    - ref: https://github.com/TryGhost/Ghost/blob/7f1d3ebc0702d8baef87fc4c09a894606940d6f7/test/unit/helpers/utils_spec.js
    - have split them and named them properly
    - did not move the git history, don't think it's useful enough to be worthwhile

---
+++ packages/helpers/test/count-images.test.js
@@ -0,0 +1,12 @@
+require('./utils');
+
+const helperUtils = ('../').utils;
+
+describe('Image Count', function () {
+    it('[success] can count images', function () {
+        var html = '<p>This is a <img src="hello.png"> text example! Count me in ;)</p><img src="hello.png">',
+            result = helperUtils.countImages(html);
+
+        result.should.equal(2);
+    });
+});
---
+++ packages/helpers/test/count-words.test.js
@@ -0,0 +1,40 @@
+require('./utils');
+
+const helperUtils = ('../').utils;
+
+describe('Word Count', function () {
+    it('[success] can count words', function () {
+        var html = 'Some words here',
+            result = helperUtils.countWords(html);
+
+        result.should.equal(3);
+    });
+
+    it('[success] sanitized HTML tags', function () {
+        var html = '<p>This is a text example! Count me in ;)</p>',
+            result = helperUtils.countWords(html);
+
+        result.should.equal(8);
+    });
+
+    it('[success] sanitized non alpha-numeric characters', function () {
+        var html = '<p>This is a text example! I love Döner. Especially number 875.</p>',
+            result = helperUtils.countWords(html);
+
+        result.should.equal(11);
+    });
+
+    it('[success] counted Chinese characters', function () {
+        var html = '<p>我今天在家吃了好多好多好吃的，现在的我非常开心非常满足</p>',
+            result = helperUtils.countWords(html);
+
+        result.should.equal(26);
+    });
+
+    it('[success] sanitized white space correctly', function () {
+        var html = ' <p> This is a text example!\n Count   me in ;)</p> ',
+            result = helperUtils.countWords(html);
+
+        result.should.equal(8);
+    });
+});
