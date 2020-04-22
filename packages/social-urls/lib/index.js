46c930e870450a5456e935d81f7ce7d3ce3fac06
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-04-22 18:21:07 -0500

    social-urls: added type hints

--- packages/social-urls/lib/index.js
+++ packages/social-urls/lib/index.js
@@ -1,8 +1,16 @@
+/**
+ * @param {string} username
+ * @returns {string}
+ */
 module.exports.twitter = function twitter(username) {
     // Creates the canonical twitter URL without the '@'
     return 'https://twitter.com/' + username.replace(/^@/, '');
 };
 
+/**
+ * @param {string} username
+ * @returns {string}
+ */
 module.exports.facebook = function facebook(username) {
     // Handles a starting slash, this shouldn't happen, but just in case
     return 'https://www.facebook.com/' + username.replace(/^\//, '');
