c353f66b309a8fcc5d098549febba546411d1522
Naz <nazargargol@gmail.com>
2021-03-05 16:46:06 +1300

    Fixed formatting in content api sdk test suite

--- packages/content-api/test/content-api-test/content-api.test.js
+++ packages/content-api/test/content-api-test/content-api.test.js
@@ -6,55 +6,53 @@ const should = require('should');
 const GhostContentApi = require('../../cjs/content-api');
 
 describe('GhostContentApi', function () {
-    describe('API v2', function () {
-        const config = {
-            url: 'https://ghost.local',
-            version: 'v2',
-            key: '0123456789abcdef0123456789'
-        };
+    const config = {
+        url: 'https://ghost.local',
+        version: 'v2',
+        key: '0123456789abcdef0123456789'
+    };
 
-        describe('new GhostContentApi', function () {
-            it('Requires a config object with url, version and key', function () {
-                try {
-                    new GhostContentApi();
-                    return should.fail();
-                } catch (err) {
-                //
-                }
+    describe('new GhostContentApi', function () {
+        it('Requires a config object with url, version and key', function () {
+            try {
+                new GhostContentApi();
+                return should.fail();
+            } catch (err) {
+            //
+            }
 
-                try {
-                    new GhostContentApi({url: config.url, version: config.version});
-                    return should.fail();
-                } catch (err) {
-                //
-                }
+            try {
+                new GhostContentApi({url: config.url, version: config.version});
+                return should.fail();
+            } catch (err) {
+            //
+            }
 
-                try {
-                    new GhostContentApi({version: config.version, key: config.key});
-                    return should.fail();
-                } catch (err) {
-                //
-                }
+            try {
+                new GhostContentApi({version: config.version, key: config.key});
+                return should.fail();
+            } catch (err) {
+            //
+            }
 
-                try {
-                    new GhostContentApi({url: config.url, key: config.key});
-                    return should.fail();
-                } catch (err) {
-                //
-                }
+            try {
+                new GhostContentApi({url: config.url, key: config.key});
+                return should.fail();
+            } catch (err) {
+            //
+            }
 
-                new GhostContentApi({host: config.url, key: config.key, version: config.version});
-                new GhostContentApi(config);
-            });
+            new GhostContentApi({host: config.url, key: config.key, version: config.version});
+            new GhostContentApi(config);
+        });
 
-            it('Returns an "api" object with posts,tags,authors&pages properties', function () {
-                const api = new GhostContentApi(config);
+        it('Returns an "api" object with posts,tags,authors&pages properties', function () {
+            const api = new GhostContentApi(config);
 
-                should.exist(api.posts);
-                should.exist(api.tags);
-                should.exist(api.authors);
-                should.exist(api.pages);
-            });
+            should.exist(api.posts);
+            should.exist(api.tags);
+            should.exist(api.authors);
+            should.exist(api.pages);
         });
     });
 });
