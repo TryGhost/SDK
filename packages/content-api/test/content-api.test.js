57f355f79465a68c33fcadae4bb9c89579ff7775
Naz <nazargargol@gmail.com>
2021-03-05 16:05:11 +1300

    Extracted server mock out of content api sdk tests
    refs https://github.com/TryGhost/SDK/commit/17e21f1867c42d75a6bf02de2b8db5c58b4db2d0
    
    - Similar reasoning to one in referenced commit.
    - This allows to reuse the server across multiple version specific test files

--- packages/content-api/test/content-api.test.js
+++ packages/content-api/test/content-api.test.js
@@ -5,6 +5,8 @@ require('./utils');
 const http = require('http');
 const url = require('url');
 const should = require('should');
+
+const {getInstance} = require('./utils/ghost-server-mock');
 const GhostContentApi = require('../');
 
 describe('GhostContentApi', function () {
@@ -15,74 +17,15 @@ describe('GhostContentApi', function () {
             key: '0123456789abcdef0123456789'
         };
 
-        let returnError;
-
         beforeEach(function () {
-            returnError = false;
+            config.returnError = false;
         });
 
         before(function (done) {
-            server = http.createServer();
-            server.on('listening', () => {
-                const {address, port} = server.address();
-                config.url = `http://${address}:${port}`;
+            server = getInstance(config, (serverURL) => {
+                config.url = serverURL;
                 done();
             });
-            server.on('request', (req, res) => {
-                const parsedUrl = url.parse(req.url, true);
-                server.emit('url', parsedUrl);
-                server.emit('headers', req.headers);
-
-                if (returnError) {
-                    res.writeHead(404, {
-                        'Content-Type': 'application/json'
-                    });
-
-                    res.end(JSON.stringify({
-                        errors: [{
-                            message: 'this is an error',
-                            context: 'this is my context',
-                            type: 'NotFoundError',
-                            details: {},
-                            help: 'docs link',
-                            code: 'ERROR',
-                            id: 'id'
-                        }]
-                    }));
-
-                    return;
-                }
-
-                res.writeHead(200, {
-                    'Content-Type': 'application/json'
-                });
-
-                let data;
-
-                const browseMatch = parsedUrl.pathname.match(/\/([a-z]+)\/$/);
-                if (browseMatch) {
-                    data = {
-                        [browseMatch[1]]: [{}, {}, {}],
-                        meta: {}
-                    };
-                }
-
-                const readByIdMatch = parsedUrl.pathname.match(/\/([a-z]+)\/([0-9a-f]+)\/$/);
-                const readBySlugMatch = parsedUrl.pathname.match(/\/([a-z]+)\/slug\/([\w]+)\/$/);
-
-                const readMatch = readByIdMatch || readBySlugMatch;
-                if (readMatch) {
-                    const type = readByIdMatch ? 'id' : 'slug';
-                    data = {
-                        [readMatch[1]]: [{
-                            [type]: readMatch[2]
-                        }]
-                    };
-                }
-
-                res.end(JSON.stringify(data));
-            });
-            server.listen(0, '127.0.0.1');
         });
 
         after(function () {
@@ -385,7 +328,7 @@ describe('GhostContentApi', function () {
                     it('request fails', function () {
                         const api = new GhostContentApi(config);
 
-                        returnError = true;
+                        config.returnError = true;
 
                         return api.authors.browse({id: 1}, 'token')
                             .then(() => {
