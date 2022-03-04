2415d5e891db5ad11546ab0b12433cf0d4229fb7
Naz <nazargargol@gmail.com>
2022-03-04 12:23:15 +0700

    Updated UrlUtils constructor documentation
    no issue
    
    - The default parameter were not documented and it's beneficial to expose them to the clients

--- packages/url-utils/lib/index.js
+++ packages/url-utils/lib/index.js
@@ -19,12 +19,12 @@ module.exports = class UrlUtils {
      * @param {Function} options.getSiteUrl
      * @param {Function} options.getAdminUrl Ghost instance admin URL
      * @param {Object} options.apiVersions configuration object which has defined `all` property which is an array of keys for other available properties
-     * @param {('v2' | 'v3' | 'v4' | 'canary')} [options.defaultApiVersion] default API version which is one of the values from options.apiVersions
-     * @param {('content' | 'admin')} [options.defaultApiType] default API type to be used and is one of the values from options.apiVersions
+     * @param {('v2' | 'v3' | 'v4' | 'canary')} [options.defaultApiVersion='v4'] default API version which is one of the values from options.apiVersions
+     * @param {('content' | 'admin')} [options.defaultApiType='content'] default API type to be used and is one of the values from options.apiVersions
      * @param {Object} [options.slugs] object with 2 properties reserved and protected containing arrays of special case slugs
      * @param {Number} [options.redirectCacheMaxAge]
-     * @param {String} [options.baseApiPath] static prefix for serving API. Should not te passed in, unless the API is being run under custom URL
-     * @param {String} [options.staticImageUrlPrefix] static prefix for serving images. Should not be passed in, unless customizing ghost instance image storage
+     * @param {String} [options.baseApiPath='/ghost/api'] static prefix for serving API. Should not te passed in, unless the API is being run under custom URL
+     * @param {String} [options.staticImageUrlPrefix='content/images'] static prefix for serving images. Should not be passed in, unless customizing ghost instance image storage
      */
     constructor(options = {}) {
         const defaultOptions = {
