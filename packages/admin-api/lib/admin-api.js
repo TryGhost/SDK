a0c4d353bf00b7f27cd264966ab9caa347a79f96
Naz <nazargargol@gmail.com>
2022-06-01 11:02:41 +0800

    Reverted "🔥 Dropped support for "host" parameter in Admin API SDK"
    reverts 931c8450609c676557b33154c1414ee75a0a201e.
    refs https://github.com/TryGhost/Toolbox/issues/348
    
    - Dropping the parameter would be breaking changes and would need a
    major release. That would be done after recent bugfixes come in and
    are released as a part of a minor bump

--- packages/admin-api/lib/admin-api.js
+++ packages/admin-api/lib/admin-api.js
@@ -44,6 +44,7 @@ const resolveAPIPrefix = (version) => {
  * @param {String|Boolean} [options.userAgent] - flag controlling if the 'User-Agent' header should be sent with a request
  * @param {Function} [options.makeRequest]
  * @param {Function} [options.generateToken]
+ * @param {String} [options.host] Deprecated
  */
 module.exports = function GhostAdminAPI(options) {
     if (this instanceof GhostAdminAPI) {
@@ -76,6 +77,20 @@ module.exports = function GhostAdminAPI(options) {
     };
 
     const config = Object.assign({}, defaultConfig, options);
+
+    //
+    /**
+     * host parameter is deprecated
+     * @deprecated use "url" instead
+     * @example new GhostAdminAPI({host: '...'})
+     */
+    if (config.host) {
+        // eslint-disable-next-line
+        console.warn(`${packageName}: The 'host' parameter is deprecated, please use 'url' instead`);
+        if (!config.url) {
+            config.url = config.host;
+        }
+    }
 
     if (config.version === undefined) {
         throw new Error(`${packageName} Config Missing: 'version' is required. E.g. ${supportedVersions.join(',')}`);
