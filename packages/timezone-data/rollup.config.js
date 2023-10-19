f88f7c0b7a82110259c79462b124b40b04b56365
Jono Mingard <reason.koan@gmail.com>
2023-10-19 16:04:09 +0100

    Updated eslint plugin to add ts config

--- packages/timezone-data/rollup.config.js
+++ packages/timezone-data/rollup.config.js
@@ -2,7 +2,7 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default [
