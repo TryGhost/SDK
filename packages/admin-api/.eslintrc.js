74e0feb21b4d15ecd56d08316a3da6d731060987
Jono Mingard <reason.koan@gmail.com>
2023-10-19 16:34:14 +0100

    Updated eslint config to ignore existing issues

--- packages/admin-api/.eslintrc.js
+++ packages/admin-api/.eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     plugins: ['ghost'],
     extends: [
         'plugin:ghost/node'
-    ]
+    ],
+    rules: {
+        'ghost/ghost-custom/no-native-error': 'off'
+    }
 };
--- packages/url-utils/.eslintrc.js
+++ packages/url-utils/.eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     plugins: ['ghost'],
     extends: [
         'plugin:ghost/node'
-    ]
+    ],
+    rules: {
+        'ghost/filenames/match-regex': 'off',
+        'ghost/filenames/match-exported-class': 'off'
+    }
 };
