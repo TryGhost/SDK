0e84d64b8c90aa72e704ca37658f0b2550356032
Jono Mingard <reason.koan@gmail.com>
2023-11-02 10:06:45 +0000

    Updated rollup config to work correctly

--- packages/limit-service/rollup.config.ts
+++ packages/limit-service/rollup.config.ts
@@ -1,4 +1,5 @@
 /* eslint-env node */
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
@@ -7,13 +8,15 @@ export default [
     // Node build
     {
         input: pkg.source,
-        external: [/node_modules/],
+        external: ['@tryghost/errors'],
         output: {
             file: pkg.main,
             format: 'cjs',
-            interop: false
+            interop: false,
+            exports: 'default'
         },
         plugins: [
+            nodeResolve({resolveOnly: ['date-fns']}),
             typescript(),
             commonjs({
                 include: ['node_modules/**', '../../node_modules/**']
@@ -24,13 +27,14 @@ export default [
     // ES module build
     {
         input: pkg.source,
-        external: [/node_modules/],
+        external: ['@tryghost/errors'],
         output: [{
             file: pkg.module,
             format: 'es',
             sourcemap: true
         }],
         plugins: [
+            nodeResolve({resolveOnly: ['date-fns']}),
             typescript(),
             commonjs({
                 include: ['node_modules/**', '../../node_modules/**']
