f6f3f38a099f10cb22ec5aac3a74dc2d7576e1f0
Ronald Langeveld <ronaldlangeveld@icloud.com>
2025-04-16 14:51:39 +0900

    Added threads social url (#510)
    ref https://linear.app/ghost/issue/PLG-393/add-new-urls-to-social-urls-sdk

--- packages/social-urls/lib/index.js
+++ packages/social-urls/lib/index.js
@@ -4,7 +4,7 @@
  */
 module.exports.twitter = function twitter(username) {
     // Creates the canonical twitter URL without the '@'
-    return 'https://twitter.com/' + username.replace(/^@/, '');
+    return 'https://x.com/' + username.replace(/^@/, '');
 };
 
 /**
@@ -14,4 +14,69 @@ module.exports.twitter = function twitter(username) {
 module.exports.facebook = function facebook(username) {
     // Handles a starting slash, this shouldn't happen, but just in case
     return 'https://www.facebook.com/' + username.replace(/^\//, '');
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.threads = function threads(username) {
+    // a threads profile url is always prefixed with @
+    username = username.replace(/^@/, '');
+    return 'https://www.threads.net/@' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.bluesky = function bluesky(username) {
+    // Bluesky URLs use profile path, no @ in stored handle
+    return 'https://bsky.app/profile/' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.mastodon = function mastodon(username) {
+    // Mastodon stores full URL without https://, just prepend protocol
+    return 'https://' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.tiktok = function tiktok(username) {
+    // TikTok URLs include @, handles stored with or without @
+    username = username.startsWith('@') ? username : '@' + username;
+    return 'https://www.tiktok.com/' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.youtube = function youtube(username) {
+    // YouTube handles include @, user/, or channel/, stored as-is
+    return 'https://www.youtube.com/' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.instagram = function instagram(username) {
+    // Instagram URLs have no @, stored without @
+    return 'https://www.instagram.com/' + username;
+};
+
+/**
+ * @param {string} username
+ * @returns {string}
+ */
+module.exports.linkedin = function linkedin(username) {
+    // LinkedIn URLs use /in/, stored without @
+    return 'https://www.linkedin.com/in/' + username;
 };
--- packages/social-urls/test/urls.test.js
+++ packages/social-urls/test/urls.test.js
@@ -2,9 +2,11 @@
 // const testUtils = require('./utils');
 require('./utils');
 
+const should = require('should');
 const social = require('../lib/index');
 
 describe('lib/social: urls', function () {
+    // Verify each platform has a URL function
     it('should have a twitter url function', function () {
         should.exist(social.twitter);
     });
@@ -13,13 +15,41 @@ describe('lib/social: urls', function () {
         should.exist(social.facebook);
     });
 
+    it('should have a threads url function', function () {
+        should.exist(social.threads);
+    });
+
+    it('should have a bluesky url function', function () {
+        should.exist(social.bluesky);
+    });
+
+    it('should have a mastodon url function', function () {
+        should.exist(social.mastodon);
+    });
+
+    it('should have a tiktok url function', function () {
+        should.exist(social.tiktok);
+    });
+
+    it('should have a youtube url function', function () {
+        should.exist(social.youtube);
+    });
+
+    it('should have an instagram url function', function () {
+        should.exist(social.instagram);
+    });
+
+    it('should have a linkedin url function', function () {
+        should.exist(social.linkedin);
+    });
+
     describe('twitter', function () {
         it('should return a correct concatenated URL', function () {
-            social.twitter('myusername').should.eql('https://twitter.com/myusername');
+            social.twitter('@myusername').should.eql('https://x.com/myusername');
         });
 
         it('should return a url without an @ sign if one is provided', function () {
-            social.twitter('@myusername').should.eql('https://twitter.com/myusername');
+            social.twitter('myusername').should.eql('https://x.com/myusername');
         });
     });
 
@@ -34,6 +64,88 @@ describe('lib/social: urls', function () {
 
         it('should return a correct concatenated URL for usernames which start with a slash', function () {
             social.facebook('/page/xxx/123').should.eql('https://www.facebook.com/page/xxx/123');
+        });
+    });
+
+    describe('threads', function () {
+        it('should return a correct concatenated URL', function () {
+            social.threads('@myusername').should.eql('https://www.threads.net/@myusername');
+        });
+
+        it('should return a correct URL for usernames without @', function () {
+            social.threads('myusername').should.eql('https://www.threads.net/@myusername');
+        });
+    });
+
+    describe('bluesky', function () {
+        it('should return a correct concatenated URL', function () {
+            social.bluesky('myusername').should.eql('https://bsky.app/profile/myusername');
+        });
+
+        it('should handle domain-based usernames', function () {
+            social.bluesky('user.domain.com').should.eql('https://bsky.app/profile/user.domain.com');
+        });
+    });
+
+    describe('mastodon', function () {
+        it('should return a correct concatenated URL for instance-specific handle', function () {
+            social.mastodon('mastodon.social/@myusername').should.eql('https://mastodon.social/@myusername');
+        });
+
+        it('should return a correct concatenated URL for federated handle', function () {
+            social.mastodon('mastodon.social/@user@instance.social').should.eql('https://mastodon.social/@user@instance.social');
+        });
+
+        it('should handle subdomains in instance URLs', function () {
+            social.mastodon('eu.mastodon.green/@eco').should.eql('https://eu.mastodon.green/@eco');
+        });
+    });
+
+    describe('tiktok', function () {
+        it('should return a correct concatenated URL', function () {
+            social.tiktok('@myusername').should.eql('https://www.tiktok.com/@myusername');
+        });
+
+        it('should return a correct URL for usernames without @', function () {
+            social.tiktok('myusername').should.eql('https://www.tiktok.com/@myusername');
+        });
+
+        it('should handle usernames with periods and underscores', function () {
+            social.tiktok('@john.smith_123').should.eql('https://www.tiktok.com/@john.smith_123');
+        });
+    });
+
+    describe('youtube', function () {
+        it('should return a correct concatenated URL for handle-based username', function () {
+            social.youtube('@myusername').should.eql('https://www.youtube.com/@myusername');
+        });
+
+        it('should return a correct concatenated URL for legacy user-based username', function () {
+            social.youtube('user/myusername').should.eql('https://www.youtube.com/user/myusername');
+        });
+
+        it('should return a correct concatenated URL for channel ID', function () {
+            social.youtube('channel/UC4QobU6STFB0P71PMvOGN5A').should.eql('https://www.youtube.com/channel/UC4QobU6STFB0P71PMvOGN5A');
+        });
+    });
+
+    describe('instagram', function () {
+        it('should return a correct concatenated URL', function () {
+            social.instagram('myusername').should.eql('https://www.instagram.com/myusername');
+        });
+
+        it('should handle usernames with periods and underscores', function () {
+            social.instagram('john.smith_123').should.eql('https://www.instagram.com/john.smith_123');
+        });
+    });
+
+    describe('linkedin', function () {
+        it('should return a correct concatenated URL', function () {
+            social.linkedin('myusername').should.eql('https://www.linkedin.com/in/myusername');
+        });
+
+        it('should handle usernames with periods, hyphens, and underscores', function () {
+            social.linkedin('john.smith-123').should.eql('https://www.linkedin.com/in/john.smith-123');
         });
     });
 });
