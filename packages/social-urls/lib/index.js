3a2e02985d3373fd4ce624ab8b5e1e7afd1516df
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-17 13:18:17 +0900

    Added support for more mastodon handles

--- packages/social-urls/lib/index.js
+++ packages/social-urls/lib/index.js
@@ -40,8 +40,33 @@ module.exports.bluesky = function bluesky(username) {
  * @returns {string}
  */
 module.exports.mastodon = function mastodon(username) {
-    // Mastodon stores full URL without https://, just prepend protocol
-    return 'https://' + username;
+    // Remove any leading @ symbols from the username
+    username = username.replace(/^@+/, '');
+
+    // Check if the input is in @username@instance format
+    if (username.includes('@') && !username.includes('/')) {
+        const [user, instance] = username.split('@');
+        return `https://${instance}/@${user}`;
+    }
+
+    // Check if the input is in instance/@username format
+    if (username.includes('/@')) {
+        return `https://${username}`;
+    }
+
+    // Check if the input is in hostInstance/@username@userInstance format
+    if (username.includes('/@') && username.includes('@', username.indexOf('/@') + 1)) {
+        return `https://${username}`;
+    }
+
+    // If we have a simple instance/username format
+    if (username.includes('/')) {
+        const [instance, user] = username.split('/');
+        return `https://${instance}/@${user}`;
+    }
+
+    // Default case: assume it's a local handle on the instance
+    return `https://${username}`;
 };
 
 /**
