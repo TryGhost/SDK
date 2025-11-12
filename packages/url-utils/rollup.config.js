a3db3a19a3b82b14c047420da6d8348f916c576b
Princi Vershwal <vershwal.princi@gmail.com>
2025-11-12 13:08:59 +0530

    Set up Rollup build configuration for TypeScript
    - Added rollup.config.js with three output formats:
      * CJS: cjs/UrlUtils.js (CommonJS for Node.js)
      * ESM: es/UrlUtils.js (ES Modules with sourcemap)
      * UMD: umd/url-utils.min.js (Browser bundle, minified)
    - Added build scripts to package.json:
      * build: Compile TypeScript and generate type definitions
      * pretest: Build before running tests
      * prepare: Build before publish
    - Updated files field to include build output directories

---
+++ packages/url-utils/rollup.config.js
@@ -0,0 +1,69 @@
+/* eslint-env node */
+import typescript from '@rollup/plugin-typescript';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+
+export default [
+    // Node build.
+    // No transpilation or bundling other than conversion from es modules to cjs
+    {
+        input: 'src/UrlUtils.ts',
+        output: {
+            file: 'cjs/UrlUtils.js',
+            format: 'cjs',
+            interop: false
+        },
+        plugins: [
+            typescript(),
+            commonjs({
+                include: ['node_modules/**', '../../node_modules/**']
+            })
+        ]
+    },
+
+    // ES module build
+    // Transpiles to es version supported by preset-env's default browsers list,
+    // bundles all necessary dependencies and polyfills
+    {
+        input: 'src/UrlUtils.ts',
+        output: [{
+            file: 'es/UrlUtils.js',
+            format: 'es',
+            sourcemap: true
+        }],
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            typescript(),
+            commonjs({
+                include: ['node_modules/**', '../../node_modules/**']
+            })
+        ]
+    },
+
+    // Standalone UMD browser build (minified).
+    // Transpiles to es version supported by preset-env's default browsers list,
+    // bundles all dependencies and polyfills.
+    {
+        input: 'src/UrlUtils.ts',
+        output: {
+            file: 'umd/url-utils.min.js',
+            format: 'umd',
+            name: 'GhostUrlUtils',
+            sourcemap: true
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            typescript(),
+            commonjs({
+                include: ['node_modules/**', '../../node_modules/**']
+            }),
+            terser()
+        ]
+    }
+];
+
