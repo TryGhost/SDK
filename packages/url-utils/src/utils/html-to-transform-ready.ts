652355f954ed403ff2c4f3b6f42eb971313ce9df
Princi Vershwal <vershwal.princi@gmail.com>
2025-11-12 13:29:12 +0530

    Renamed utility files to .ts with @ts-nocheck
    - This prepares files for TypeScript migration. No code changes, pure rename.

---
+++ packages/url-utils/src/utils/html-to-transform-ready.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+const htmlRelativeToAbsolute = require('./html-relative-to-absolute');
+const htmlAbsoluteToTransformReady = require('./html-absolute-to-transform-ready');
+
+function htmlToTransformReady(html, siteUrl, itemPath, options) {
+    if (typeof itemPath === 'object' && !options) {
+        options = itemPath;
+        itemPath = null;
+    }
+    const absolute = htmlRelativeToAbsolute(html, siteUrl, itemPath, options);
+    return htmlAbsoluteToTransformReady(absolute, siteUrl, options);
+}
+
+module.exports = htmlToTransformReady;
