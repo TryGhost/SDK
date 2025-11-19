91d9c5c61e070bfbc5f95eeccf017d99ae1cad1d
Fabien O'Carroll <fabien@allou.is>
2025-11-19 11:10:12 +0700

    Fixed early exit for markdown transform
    Fixed critical bug where markdown-absolute-to-transform-ready only checked
    siteUrl in early exit logic, causing content with ONLY CDN URLs to be
    incorrectly skipped.
    
    Added buildEarlyExitMatch() function (duplicated from HTML implementation)
    that builds regex pattern matching ANY configured base URL:
    - siteUrl
    - imageBaseUrl  
    - mediaBaseUrl
    - filesBaseUrl

--- packages/url-utils/lib/utils/markdown-absolute-to-transform-ready.js
+++ packages/url-utils/lib/utils/markdown-absolute-to-transform-ready.js
@@ -2,12 +2,36 @@ const markdownTransform = require('./markdown-transform');
 const absoluteToTransformReady = require('./absolute-to-transform-ready');
 const htmlAbsoluteToTransformReady = require('./html-absolute-to-transform-ready');
 
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function buildEarlyExitMatch(siteUrl, options) {
+    const candidates = [siteUrl, options.imageBaseUrl, options.filesBaseUrl, options.mediaBaseUrl]
+        .filter(Boolean)
+        .map((value) => {
+            let normalized = options.ignoreProtocol ? value.replace(/http:|https:/, '') : value;
+            return normalized.replace(/\/$/, '');
+        })
+        .filter(Boolean)
+        .map(escapeRegExp);
+
+    if (!candidates.length) {
+        return null;
+    }
+
+    if (candidates.length === 1) {
+        return candidates[0];
+    }
+
+    return `(?:${candidates.join('|')})`;
+}
+
 function markdownAbsoluteToTransformReady(markdown = '', siteUrl, _options = {}) {
     const defaultOptions = {assetsOnly: false, ignoreProtocol: true};
     const options = Object.assign({}, defaultOptions, _options);
 
-    options.earlyExitMatchStr = options.ignoreProtocol ? siteUrl.replace(/http:|https:/, '') : siteUrl;
-    options.earlyExitMatchStr = options.earlyExitMatchStr.replace(/\/$/, '');
+    options.earlyExitMatchStr = buildEarlyExitMatch(siteUrl, options);
 
     // need to ignore itemPath because absoluteToTransformReady functions doen't take that option
     const transformFunctions = {
