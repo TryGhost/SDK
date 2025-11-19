f3d7a8285e0e1a98c57e3f8544725c4623be142e
Princi Vershwal <vershwal.princi@gmail.com>
2025-11-19 14:49:17 +0530

    Configured ESLint to ignore linting errors for TypeScript files with @ts-nocheck
    - Allowed @ts-nocheck comments in TypeScript files
    - Disabled @typescript-eslint/no-var-requires for .ts files
    - Disabled prefer-const rule for .ts files
    
    This ensures linting passes while TypeScript migration is in progress.

--- packages/url-utils/.eslintrc.js
+++ packages/url-utils/.eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
     plugins: ['ghost'],
     extends: [
-        'plugin:ghost/node'
+        'plugin:ghost/ts'
+    ],
+    overrides: [
+        {
+            // Files with @ts-nocheck are intentionally not fully migrated yet
+            // Disable all linting rules that would fail on these files
+            files: ['**/*.ts'],
+            rules: {
+                '@typescript-eslint/ban-ts-comment': ['error', {
+                    'ts-nocheck': false
+                }],
+                '@typescript-eslint/no-var-requires': 'off',
+                'prefer-const': 'off'
+            }
+        }
     ]
 };
