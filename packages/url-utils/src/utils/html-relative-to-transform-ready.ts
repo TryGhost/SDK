410299f592bec8b2a3263cfd7c675b1d7f8e3eac
Fabien O'Carroll <fabien@allou.is>
2026-01-06 14:56:45 +0700

    Deduped attribute list for html transformation
    no-issue
    
    The early exit condition doesn't actually match the attributes which we
    were transforming (`style` is missing), by having a single source of
    truth we can easily fix the bug and prevent issues like this in future.

--- packages/url-utils/src/utils/html-relative-to-transform-ready.ts
+++ packages/url-utils/src/utils/html-relative-to-transform-ready.ts
@@ -1,5 +1,5 @@
 import type {HtmlTransformOptionsInput} from './types';
-import htmlTransform from './html-transform';
+import htmlTransform, {earlyExitMatchStr} from './html-transform';
 import relativeToTransformReady, {type RelativeToTransformReadyOptionsInput as RelativeToTransformReadyOptionsInputType} from './relative-to-transform-ready';
 
 const htmlRelativeToTransformReady = function (
@@ -27,8 +27,7 @@ const htmlRelativeToTransformReady = function (
     };
     const options: HtmlTransformOptionsInput = Object.assign({}, defaultOptions, finalOptions, overrideOptions);
 
-    // exit early and avoid parsing if the content does not contain an attribute we might transform
-    options.earlyExitMatchStr = 'href=|src=|srcset=';
+    options.earlyExitMatchStr = earlyExitMatchStr;
     if (options.assetsOnly) {
         options.earlyExitMatchStr = options.staticImageUrlPrefix;
     }
