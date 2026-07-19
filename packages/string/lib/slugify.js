8cf91ee8534c04091508c7fbbad81ddfd4df9069
dittnamn <gh@dittnamn.com>
2026-07-19 09:07:41 +0200

    🎨 Changed and improved the slug generation
    ref towards https://github.com/TryGhost/Ghost/issues/3224 etc.
    The old slug generation using unidecode had a lot of issues with
    erroneous transliteration for many languages, which has been the reason
    for many discussions and requests for change over the years. By
    replacing unidecode with anyascii, a lot of these issues should be
    solved, however not perfectly.
    
    Over the years, the initial reasons for not allowing anything other than
    ascii letters and numbers in the slugs have been fixed. The Ghost
    databases, routes, links, loading, etc. now support unicode characters
    in URL:s. Browser support is fully working and many large sites,
    including Wikipedia, use unicode characters in URL:s.
    
    With just a few modifications in the Ghost sources, an option for full
    unicode slug support could therefore be added for users who want it. To
    work towards this, an extra option has been added to the slugify
    function, to allow the disabling of the transliteration part of the slug
    generation.
    
    Another option, allowing the change of slug part separator was also
    added. Due to how the filtering work, the possible options are currently
    just spaces, dashes and underscores, but more options could possibly be
    added in the future. Dots as separators could in theory make good
    looking slugs, but should be avoided due to the risk of filename mixups.
    
    Due to the slightly different transliteration method, some of the tests
    have been revised and some new ones were added as well. Note that
    anyascii has some quirks compared to unidecode, but in total this is an
    improvement for most languages.

--- packages/string/lib/slugify.js
+++ packages/string/lib/slugify.js
@@ -1,4 +1,4 @@
-const unidecode = require('unidecode');
+const anyAscii = require('any-ascii').default;
 const stripInvisibleChars = require('./strip-invisible-chars');
 
 /**
@@ -9,40 +9,51 @@ const stripInvisibleChars = require('./strip-invisible-chars');
  * @param {String} string - the string we want to slugify
  * @param {object} options - filter options
  * @param {bool} [options.requiredChangesOnly] - don't perform optional cleanup, e.g. removing extra dashes
+ * @param {bool} [options.noTransliteration] - don't perform optional transliteration, e.g. keep smörgåsbord as it is instead of turning it into smorgasbord
+ * @param {string} [options.separator] - separator to be used for the slugs, can be ` `, `_` or `-`, defaults to `-`
  * @returns {String} slugified string
  */
 module.exports = function (string, options = {}) {
+    // If the separator isn't set, default to `-`
+    const separator = options.separator || '-';
+
     // Ensure we have a string
     string = string || '';
 
     // Strip all characters that cannot be printed
-    string = stripInvisibleChars(string);
-
-    // Handle the £ symbol separately, since it needs to be removed before the unicode conversion.
-    string = string.replace(/£/g, '-');
-
-    // Remove non ascii characters
-    string = unidecode(string);
-
-    // Replace URL reserved chars: `@:/?#[]!$&()*+,;=` as well as `\%<>|^~£"{}` and \`
-    string = string.replace(/(\s|\.|@|:|\/|\?|#|\[|\]|!|\$|&|\(|\)|\*|\+|,|;|=|\\|%|<|>|\||\^|~|"|\{|\}|`|–|—)/g, '-')
+    string = stripInvisibleChars(string)
     // Remove apostrophes
         .replace(/'/g, '')
+        // Remove anything that's not a letter, number or a separator
+        .replace(/[^\p{L}\p{N}\s_-]/gu, separator);
+
+    // Perform the transliteration if requested
+    if (!options.noTransliteration) {
+        string = anyAscii(string);
+    }
+
+    // Replace spaces, URL reserved chars: `@:/?#[]!$&()*+,;=` as well as `\%<>|^~£"{}-` and \` with the selected separator.
+    // Should only be needed in case the transliteration added something, but it's safer to always run in case the regex filter missed something.
+    string = string.replace(/(\s|\.|@|:|\/|\?|#|\[|\]|!|\$|&|\(|\)|\*|\+|,|;|=|\\|%|<|>|\||\^|~|£|"|\{|\}|`|–|—)/g, separator)
+    // Remove apostrophes (again, in case the transliteration added some)
+        .replace(/'/g, '')
+        // camelCase looking text after initial cleanup and transliteration are most likely separate words, so we add separators between the parts
+        .replace(/([a-z])([A-Z])/g, `$1${separator}$2`)
         // Make the whole thing lowercase
         .toLowerCase();
 
     // These changes are optional changes, we can enable/disable these
     if (!options.requiredChangesOnly) {
-        // Convert 2 or more dashes into a single dash
-        string = string.replace(/-+/g, '-')
-        // Remove trailing dash
-            .replace(/-$/, '')
-            // Remove any dashes at the beginning
-            .replace(/^-/, '');
+        // Convert 2 or more separators into a single separator
+        string = string.replace(/[\s_-]{2,}/g, separator)
+        // Remove trailing separators
+            .replace(/[\s_-]$/, '')
+            // Remove any separators at the beginning
+            .replace(/^[\s_-]/, '');
+    } else {
+        // Handle whitespace at the beginning or end.
+        string = string.trim();
     }
-
-    // Handle whitespace at the beginning or end.
-    string = string.trim();
 
     return string;
 };
